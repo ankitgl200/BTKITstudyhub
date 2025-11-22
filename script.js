4805f6e7dadf7a646baa11346f2c53c72f360d8d
ankitgl200 <ukankitpro@gmail.com>
2025-11-22 17:02:49 +0530

    Implement filter functionality for note cards
    Added filtering functionality for note cards based on button clicks.

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
         yearSpan.textContent = new Date().getFullYear();
     }
 });
+
+
+// filter
+document.addEventListener("DOMContentLoaded", () => {
+
+    const filterButtons = document.querySelectorAll(".filter-btn");
+    const cards = document.querySelectorAll(".note-card");
+
+    filterButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const filter = btn.dataset.filter;
+
+            // update active button
+            filterButtons.forEach(b => b.classList.remove("active"));
+            btn.classList.add("active");
+
+            cards.forEach(card => {
+                const subject = card.dataset.subject;
+
+                if (filter === "all" || filter === subject) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    });
+
+});
